34bb8c780eb4750a0fab536dea06c02cd2102a80
sonam <sonamsahu199518@gmail.com>
2022-04-12 11:18:19 +0530

    Add project files.

---
+++ ServerSideValidation/Controllers/StudentController.cs
@@ -0,0 +1,30 @@
+﻿using ServerSideValidation.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ServerSideValidation.Controllers
+{
+    public class StudentController : Controller
+    {
+        // GET: Student
+        public ActionResult Index()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Index(Student student)
+        {
+            if (ModelState.IsValid)
+            {
+                ViewBag.Name = student.Name;
+                ViewBag.Email = student.Email;
+            }
+            return View(student);
+        }
+
+
+    }
+}
---
+++ ServerSideValidation/Models/Student.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;//required namespace
+using System.Linq;
+using System.Web;
+
+namespace ServerSideValidation.Models
+{
+    public class Student
+    {
+        [Required(ErrorMessage = "Name Is Required")]
+        public string Name { get; set; }
+        [Required(ErrorMessage = "Email Is Required")]
+        [RegularExpression(@"^([a-zA-Z0-9_\-\.]+)@((\[[0-9]{1,3}" +  
+                            @"\.[0-9]{1,3}\.[0-9]{1,3}\.)|(([a-zA-Z0-9\-]+\" +  
+                            @".)+))([a-zA-Z]{2,4}|[0-9]{1,3})(\]?)$",  
+                            ErrorMessage = "Email is not valid")] 
+        public string Email { get; set; }
+    }
+}
